7e0e02e192b599763533b543aa176179de902e8d
Aasa-Christian <aasachristian@yahoo.com>
2021-06-13 19:39:23 -0400

    created landingheadercont styled-component

--- src/Components/StyledComponents.js
+++ src/Components/StyledComponents.js
@@ -31,3 +31,10 @@ background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/s
 opacity: 35%;
 position: sticky;
 `;
+export const LandingHeaderCont = styled.section`
+height: 100%;
+width: 100%;
+top: 0;
+z-index: 1;
+position: sticky;
+`;
