b32ecad8c12deb6d9b4b7b0cc817f924445c6fc4
Aasa-Christian <aasachristian@yahoo.com>
2021-06-14 22:21:54 -0400

    created dummy data to populate site in development

---
+++ src/DummyData.js
@@ -0,0 +1,16 @@
+export const data = {
+  articles: [
+    {
+      title: "Your Personal Trainer",
+      body: "Personal Training is more than just grinding reps in the gym. Training Majors offer guidence on your diet, recovery and general wellness.",
+    },
+    {
+      title: "Your Personal Plan",
+      body: "Everyone's fitness journey is different. Training Majors creates a specific plan for you based on your own goals. ",
+    },
+    {
+      title: "Your Personal Time",
+      body: "It can be difficult being new to working out. Training Majors will take the time to help you build a solid foundation first, than encrease the intensity in a safe and productive manner ",
+    },
+  ],
+};
