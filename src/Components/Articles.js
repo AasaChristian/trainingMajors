770f51885ef5ed0f9f9f0ff005d755832f8542f6
Aasa-Christian <aasachristian@yahoo.com>
2021-06-20 17:17:30 -0400

    updated styling

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -1,16 +1,22 @@
 
 import React from 'react';
 import Fade from 'react-reveal/Fade';
+import aceBack from '../img/aceBack.jpg'
+import aceDeadlift from '../img/aceDeadlift.jpg'
+
 
 function Articles(props) {
 const {index, title, body} = props
 
 console.log(index % 2, "tw0")
     return (
-        <section key={index} style={{height: "30%", width: "100%" ,display: 'flex', justifyContent: 'center' }}>
-          <Fade left={index % 2 === 0? true : false} right={index % 2 === 1? true : false}>        <div style={ index % 2 === 0 ? {height: "100%", width: "70%" ,border: "solid black 5px", backgroundColor: '#cf9ea7', borderTopRightRadius: '8%', borderBottomRightRadius: '8%'} : {height: "100%", width: "70%" ,border: "solid black 5px", backgroundColor: '#8DA9C4', borderBottomLeftRadius: '8%', borderTopLeftRadius: '8%'}}>
-          <h1>{title}</h1>
-          <h3>{body}</h3>
+        <section key={index} style={{height: "35%", width: "100%" ,display: 'flex', justifyContent: 'center' , margin: '2%'}}>
+          <Fade left={index % 2 === 0? true : false} right={index % 2 === 1? true : false}>        <div style={ index % 2 === 0 ? {height: "100%", width: "70%" ,border: "solid black 5px", backgroundImage: `url(${aceBack})` , backgroundSize: 'cover', borderTopRightRadius: '8%', borderBottomRightRadius: '8%'} : {height: "100%", width: "70%" ,border: "solid black 5px", backgroundImage: `url(${aceDeadlift})`, backgroundSize: 'cover', borderBottomLeftRadius: '8%', borderTopLeftRadius: '8%'}}>
+          <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>          
+            <h1 style={{background: 'none', borderRadius: '10%', border: 'white solid 4px', color: 'white', fontSize: '350%', padding: '2%', marginBottom: '2%', marginTop: '4%'}}>{title}</h1>
+          <h3 style={{display: 'flex', width: '85%', color: 'white', fontSize: '150%', marginTop: '0%'}}>{body}</h3>
+          </div>
+
         </div>
         </Fade>
 
