10112e391ba6887f60abe930e2fa21651942c4d8
Aasa-Christian <aasachristian@yahoo.com>
2021-06-20 20:17:23 -0400

    styling

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -20,7 +20,7 @@ console.log(index % 2, "tw0")
 
 
 
-          <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>          
+          <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', height: '90%', justifyContent: 'space-evenly'}}>          
             <ArticleTitle >{title}</ArticleTitle>
             <div style={{display: 'flex', justifyContent: 'center'}}>
               <ArticleBody >{body}</ArticleBody>
