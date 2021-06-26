c0ac680784bf205e15e8a5c78c5937e4764613fa
Aasa-Christian <aasachristian@yahoo.com>
2021-06-26 16:17:30 -0400

    added background and header to contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,11 +1,38 @@
-
+import {LandingBodyBackground, LandingBody, LandingBodyScroll, LandingPositionFixed,LandingHeaderCont, MajorText} from './StyledComponents'
+import Header from './Header'
 
 
 function Contact() {
+
+  const contacts = []
   return (
-<div>
-    <h1>CONTCTS</h1>
-</div>
+    <div>
+    <LandingPositionFixed>
+
+    <LandingBodyScroll> 
+    <LandingHeaderCont>
+   <Header/>
+      </LandingHeaderCont>
+      </ LandingBodyScroll>
+    </LandingPositionFixed>
+        <LandingBody>
+        <MajorText style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>Training Majors</MajorText>
+        <div style={{width: "98%", display: 'flex', flexDirection: 'column', justifyContent: "space-evenly", height: '95%', paddingLeft: '2%'}}>
+          {contacts.map((articles, i) => {
+    console.log(articles, "articles")
+    return(
+      // <Articles
+      // index={i}
+      // title={articles.title}
+      // body ={articles.body}/>
+      <h1>articles</h1>
+    )
+  })}
+  </div>
+  
+  
+    </LandingBody>
+    </div>
   );
 }
 
