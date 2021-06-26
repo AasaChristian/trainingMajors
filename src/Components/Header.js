355257ad8e88b594e923b657f810b3a93261bdcd
Aasa-Christian <aasachristian@yahoo.com>
2021-06-26 16:46:41 -0400

    removed login button

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ function Header(props) {
 const {contactHome, linkTo } = props
   return (
     <div style={{display: "flex", flexDirection: 'row', justifyContent: "space-around", zIndex: '1'}}>
-<h1>Login</h1>
+{/* <h1>Login</h1> */}
 <h1>Schedule session</h1>
 <NavLink to={linkTo}><h1>{contactHome}</h1></NavLink>
 
